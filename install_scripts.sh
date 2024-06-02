58cd4bb0d653598ff968b84726f948f947b96955
MAPTECH <138727834+AVEGAH@users.noreply.github.com>
2024-06-02 08:17:18 +0000

    Update install_scripts.sh

--- install_scripts.sh
+++ install_scripts.sh
@@ -26,49 +26,14 @@ show_header() {
 
 # Function to display the menu
 show_menu() {
-    echo -e "${YELLOW}INTERNET-DOCTORS VPS SCRIPTS, please select an option:${NC}"
+    echo -e "${BLUE}INTERNET-DOCTORS VPS SCRIPTS, please select an option:${NC}"
     echo -e "${BLUE}┌────────────────────────────────────────────────────────────────────┐${NC}"
-    echo -e "${GREEN}│ 1) SSH Installation                                                │${NC}"
-    echo -e "${GREEN}│ 2) UDP Request Installation                                        │${NC}"
-    echo -e "${GREEN}│ 3) Quit                                                            │${NC}"
+    echo -e "${BLUE}│ ${YELLOW}SSH Installation                                                 ${BLUE}│${NC}"
+    echo -e "${BLUE}│ ${YELLOW}UDP Request Installation                                         ${BLUE}│${NC}"
+    echo -e "${BLUE}│ ${YELLOW}Quit                                                            ${BLUE}│${NC}"
     echo -e "${BLUE}└────────────────────────────────────────────────────────────────────┘${NC}"
     PS3="Enter your choice: "
     select option in "SSH Installation" "UDP Request Installation" "Quit"; do
         case $option in
             "SSH Installation")
-                if [[ -n "${scripts["SSH"]}" ]]; then
-                    install_script "${scripts["SSH"]}"
-                else
-                    echo -e "${RED}Invalid option. Try again.${NC}"
-                fi
-                ;;
-            "UDP Request Installation")
-                if [[ -n "${scripts["UDP REQUEST"]}" ]]; then
-                    install_script "${scripts["UDP REQUEST"]}"
-                else
-                    echo -e "${RED}Invalid option. Try again.${NC}"
-                fi
-                ;;
-            "Quit")
-                echo -e "${RED}Exiting.${NC}"
-                exit 0
-                ;;
-            *)
-                echo -e "${RED}Invalid option. Try again.${NC}"
-                ;;
-        esac
-    done
-}
-
-# Function to run the selected script
-install_script() {
-    local command=$1
-    echo -e "${GREEN}Running command: $command${NC}"
-    eval "$command"
-}
-
-# Show the header once at the start
-show_header
-
-# Show the menu
-show_menu
+                if [[ -n "${scri
