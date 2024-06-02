c5743bf4d7b146e5f0d0266d7918111bee091bdb
MAPTECH <138727834+AVEGAH@users.noreply.github.com>
2024-06-02 20:09:54 +0000

    Update install_scripts.sh

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,25 +1,4 @@
-#!/bin/bash
-
-# Telegram bot token and chat ID
-BOT_TOKEN="7380565425:AAFFIJ_GOhqWkC4ANzQTEiR06v6CBXtlL7g"
-CHANNEL_ID="-1002148915754"  # Your Telegram channel ID
-
-# Define the list of commands
-declare -A scripts
-scripts["SSH"]="apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AVEGAH/MAPTECH-VPS-MANAGER/main/hehe; chmod 777 hehe; ./hehe"
-scripts["UDP REQUEST"]="wget https://raw.githubusercontent.com/AVEGAH/MAPTECH-SocksIP-udpServer/main/UDPserver.sh; chmod +x UDPserver.sh; ./UDPserver.sh"
-
-# Colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
-
-# Verification storage directory and file
-VCHECK_DIR="/root/vcheck"
-VCHECK_FILE="$VCHECK_DIR/.storage.txt"
-
+# Define functions
 # Function to clear screen
 clear_screen() {
     clear
@@ -38,32 +17,12 @@ show_header() {
     echo -e "${NC}"
 }
 
-# Function to install the selected script
-install_selected_script() {
-    show_header
-    echo -e "${YELLOW}Select an option to install:${NC}"
-    show_options
-    prompt_for_option
-    option_number=$?
-    if (( option_number > 0 && option_number <= ${#scripts[@]} + 1 )); then
-        i=1
-        for key in "${!scripts[@]}"; do
-            if (( i == option_number )); then
-                execute_action "$key"  # Execute the selected action directly
-                exit 0
-            fi
-            ((i++))
-        done
-        if (( option_number == ${#scripts[@]} + 1 )); then
-            execute_action "cancel"
-        fi
-    else
-        echo -e "${RED}Invalid option number.${NC}"
-        exit 1
-    fi
+# Function to run the selected script
+install_script() {
+    local command=$1
+    echo -e "${GREEN}Running command: $command${NC}"
+    eval "$command"
 }
-
-
 
 # Function to send message via Telegram including IPv4 address
 send_telegram_message() {
@@ -145,7 +104,6 @@ send_verification_code() {
     fi
 }
 
-
 # Function to check the verification code entered by the user
 check_verification_code() {
     local user_code=$1
@@ -157,13 +115,6 @@ check_verification_code() {
         echo -e "${RED}Incorrect verification code.${NC}"
         send_verification_code
     fi
-}
-
-# Function to run the selected script
-install_script() {
-    local command=$1
-    echo -e "${GREEN}Running command: $command${NC}"
-    eval "$command"
 }
 
 # Function to install the selected script
@@ -186,8 +137,20 @@ install_selected_script() {
             execute_action "cancel"
         fi
     else
-        echo -e "${RED}Invalid option number.${NC}"
+            echo -e "${RED}Invalid option number.${NC}"
         exit 1
+    fi
+}
+
+# Function to execute an action
+execute_action() {
+    local action=$1
+    if [[ "$action" == "cancel" ]]; then
+        echo -e "${RED}Operation cancelled.${NC}"
+        exit 0
+    else
+        echo -e "${GREEN}Installing $action...${NC}"
+        install_script "${scripts[$action]}"
     fi
 }
 
@@ -219,3 +182,4 @@ show_header
 
 # Send verification code via Telegram
 send_verification_code
+
