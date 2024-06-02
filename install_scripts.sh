891e87c592a3f23ed12dffd1191e18ff5de9a87b
MAPTECH <138727834+AVEGAH@users.noreply.github.com>
2024-06-02 08:05:17 +0000

    Add files via upload

---
+++ install_scripts.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Define the list of commands
+declare -A scripts
+scripts["SSH"]="apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AVEGAH/MAPTECH-VPS-MANAGER/main/hehe; chmod 777 hehe; ./hehe"
+scripts["UDP REQUEST"]="wget https://raw.githubusercontent.com/AVEGAH/MAPTECH-SocksIP-udpServer/main/UDPserver.sh; chmod +x UDPserver.sh; ./UDPserver.sh"
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# ASCII Art Header
+show_header() {
+    echo -e "${BLUE}"
+    echo ".___  ___.      ___      .______   .___________. _______   ______  __    __  "
+    echo "|   \/   |     /   \     |   _  \  |           ||   ____| /      ||  |  |  | "
+    echo "|  \  /  |    /  ^  \    |  |_)  | `---|  |----`|  |__   |  ,----'|  |__|  | "
+    echo "|  |\/|  |   /  /_\  \   |   ___/      |  |     |   __|  |  |     |   __   | "
+    echo "|  |  |  |  /  _____  \  |  |          |  |     |  |____ |  `----.|  |  |  | "
+    echo "|__|  |__| /__/     \__\ | _|          |__|     |_______| \______||__|  |__| "
+    echo -e "${NC}"
+}
+
+# Function to display the menu
+show_menu() {
+    echo -e "${YELLOW}Choose a script to install:${NC}"
+    PS3="Enter your choice: "
+    select option in "${!scripts[@]}" "Quit"; do
+        case $option in
+            "Quit")
+                echo -e "${RED}Exiting.${NC}"
+                exit 0
+                ;;
+            *)
+                if [[ -n "${scripts[$option]}" ]]; then
+                    install_script "${scripts[$option]}"
+                else
+                    echo -e "${RED}Invalid option. Try again.${NC}"
+                fi
+                ;;
+        esac
+    done
+}
+
+# Function to run the selected script
+install_script() {
+    local command=$1
+    echo -e "${GREEN}Running command: $command${NC}"
+    eval "$command"
+}
+
+# Show the header once at the start
+show_header
+
+# Show the menu
+show_menu
