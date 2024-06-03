268887f853e051b8d2ea174d9bacbbe093cefb12
MAPTECH <138727834+AVEGAH@users.noreply.github.com>
2024-06-03 01:37:58 +0000

    Update b.sh

--- b.sh
+++ b.sh
@@ -6,11 +6,11 @@ CHANNEL_ID="-1002148915754"  # Your Telegram channel ID
 
 # Define the list of commands
 declare -A scripts
-scripts["SSH"]="apt-get update -y; apt-get upgrade -y; wget https://tinyurl.com/2n8nch9w; chmod 777 hehe; ./hehe"
-scripts["UDP REQUEST"]="wget https://tinyurl.com/2h9bvm5r; chmod +x UDPserver.sh; ./UDPserver.sh"
-scripts["UDP CUSTOM"]="git clone https://tinyurl.com/2zr5b92m && cd Udpcustom && chmod +x install.sh && ./install.sh"
-scripts["UDP HYSTERIA"]="wget https://tinyurl.com/4h3r9jv2; chmod +x install_agnudp.sh; ./install_agnudp.sh; nano /etc/hysteria/config.json"
-scripts["HIDDIFY NEXT"]="bash <(curl -Ls https://tinyurl.com/2c2y4k9h)"
+scripts["SSH"]="apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AVEGAH/MAPTECH-VPS-MANAGER/main/hehe; chmod 777 hehe; ./hehe"
+scripts["UDP REQUEST"]="wget https://raw.githubusercontent.com/AVEGAH/SocksIP-udpServer/main/UDPserver.sh; chmod +x UDPserver.sh; ./UDPserver.sh"
+scripts["UDP CUSTOM"]="git clone https://github.com/AVEGAH/Udpcustom.git && cd Udpcustom && chmod +x install.sh && ./install.sh"
+scripts["UDP HYSTERIA"]="wget https://github.com/khaledagn/AGN-UDP/raw/main/install_agnudp.sh; chmod +x install_agnudp.sh; ./install_agnudp.sh; nano /etc/hysteria/config.json"
+scripts["HIDDIFY NEXT"]="bash <(curl -Ls https://raw.githubusercontent.com/ozipoetra/z-ui/main/install.sh)"
 scripts["Autoscript"]="sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/AVEGAH/AutoScriptXray/master/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh"
 
 
