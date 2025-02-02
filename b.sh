fc38594d8e011055b75804d95672fec4c59c6c20
MAPTECH <138727834+AVEGAH@users.noreply.github.com>
2025-02-02 12:16:33 +0000

    Update b.sh

--- b.sh
+++ b.sh
@@ -13,6 +13,7 @@ scripts["UDP CUSTOM"]="git clone https://github.com/AVEGAH/Udpcustom.git && cd U
 scripts["UDP HYSTERIA"]="wget https://github.com/khaledagn/AGN-UDP/raw/main/install_agnudp.sh; chmod +x install_agnudp.sh; ./install_agnudp.sh; nano /etc/hysteria/config.json"
 scripts["HIDDIFY NEXT"]="bash <(curl -Ls https://raw.githubusercontent.com/ozipoetra/z-ui/main/install.sh)"
 scripts["Autoscript"]="sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/AVEGAH/AutoScriptXray/master/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh"
+scripts["SlowDNS"]="rm -rf install; apt update; wget https://github.com/AVEGAH/SlowDNS/raw/main/install; chmod 777 install; ./install --start"
 
 # Colors
 RED='\033[0;31m'
